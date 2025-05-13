1b378e43376b79f444a776fc9bd331573849d700
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-13 23:05:45 +0530

    Remove commented explanations for character transformations in lengthAfterTransformations function

--- Day_178_3335_Total_Characters_in_String_After_Transformations_I.cpp
+++ Day_178_3335_Total_Characters_in_String_After_Transformations_I.cpp
@@ -12,10 +12,8 @@ public:
 
     while (t-- > 0) {
       vector<int> newCount(26);
-      // 'a' -> 'b', 'b' -> 'c', ..., 'y' -> 'z'
       for (int i = 0; i < 25; ++i)
         newCount[i + 1] = count[i];
-      // 'z' -> 'ab'
       newCount[0] = count[25];
       newCount[1] = (newCount[1] + count[25]) % kMod;
       count = std::move(newCount);
