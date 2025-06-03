3f04b8e3e0340c7acc71667a2daa48e73ca21981
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-03 16:16:40 +0530

    Refactor code structure by adding missing includes and comments for clarity in maxCandies function

--- Day_199_1298_Maximum_Candies_You_Can_from_Boxes.cpp
+++ Day_199_1298_Maximum_Candies_You_Can_from_Boxes.cpp
@@ -1,3 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// This code defines a function to calculate the maximum candies that can be collected from boxes,
+// given the constraints of keys and contained boxes. It uses a greedy approach to open boxes
+// and collect candies until no more boxes can be opened.
+
 class Solution {
 public:
     int maxCandies(vector<int>& vis, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
