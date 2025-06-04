3a0338cdb0be186491fdc25c617d327b3009c473
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-04 23:41:29 +0530

    Enhance comments for clarity in answerString function implementation

--- Day_200_3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.cpp
+++ Day_200_3403_Find_the_Lexicographically_Largest_String_From_the_Box_I.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 class Solution {
 public:
+    // Returns the lexicographically largest substring that can be formed
+    // by removing (numFriends - 1) characters from the input string 'word'.
     string answerString(string word, int numFriends) {
+        // If only one friend, return the whole word
         if (numFriends == 1) return word;
         string res = "";
+        // Iterate through all possible starting positions
         for (int i = 0 ; i < word.size() ; i++) 
+            // For each position, consider the substring of length (word.length() - numFriends + 1)
+            // and update 'res' if this substring is lexicographically larger
             res = max(res, word.substr(i, word.length() - numFriends + 1));
         return res;
     }
