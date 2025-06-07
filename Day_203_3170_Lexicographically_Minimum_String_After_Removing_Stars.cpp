781c5f6d75b3e40d5a3f99101cc2740c196f986c
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-07 23:25:41 +0530

    Refactor clearStars function for improved readability and consistency in formatting

--- Day_203_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
+++ Day_203_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
@@ -1,21 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+// This code defines a function to clear stars from a string and return the lexicographically smallest string
+// by replacing stars with the last character in the corresponding bucket of characters.
+
 class Solution {
 public:
     string clearStars(string s) {
          string ans = s;
-    vector<vector<int>> buckets(26);
+        vector<vector<int>> buckets(26);
 
-    for (int i = 0; i < s.length(); ++i)
-      if (s[i] == '*') {
-        ans[i] = ' ';
-        int j = 0;
-        while (buckets[j].empty())
-          ++j;
-        ans[buckets[j].back()] = ' ', buckets[j].pop_back();
-      } else {
-        buckets[s[i] - 'a'].push_back(i);
-      }
+        for (int i = 0; i < s.length(); ++i)
+        if (s[i] == '*') {
+            ans[i] = ' ';
+            int j = 0;
+            while (buckets[j].empty())
+            ++j;
+            ans[buckets[j].back()] = ' ', buckets[j].pop_back();
+        } else {
+            buckets[s[i] - 'a'].push_back(i);
+        }
 
-    std::erase(ans, ' ');
-    return ans;
+        std::erase(ans, ' ');
+        return ans;
     }
 };
