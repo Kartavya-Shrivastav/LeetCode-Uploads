887941efdc66fb3ae21faf8508464e2144d168b1
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-07 23:26:13 +0530

    Refactor clearStars function for improved readability and maintainability

--- Day_203_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
+++ Day_203_3170_Lexicographically_Minimum_String_After_Removing_Stars.cpp
@@ -5,22 +5,28 @@ using namespace std;
 
 class Solution {
 public:
+    // Function to remove stars and return the lexicographically smallest string
     string clearStars(string s) {
-         string ans = s;
-        vector<vector<int>> buckets(26);
+        string ans = s; // Copy of the input string to modify
+        vector<vector<int>> buckets(26); // Buckets to store indices of each character
 
-        for (int i = 0; i < s.length(); ++i)
-        if (s[i] == '*') {
-            ans[i] = ' ';
-            int j = 0;
-            while (buckets[j].empty())
-            ++j;
-            ans[buckets[j].back()] = ' ', buckets[j].pop_back();
-        } else {
-            buckets[s[i] - 'a'].push_back(i);
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '*') {
+                ans[i] = ' '; // Mark the star for removal
+                int j = 0;
+                // Find the smallest character bucket that is not empty
+                while (buckets[j].empty())
+                    ++j;
+                // Remove the last occurrence of the smallest character
+                ans[buckets[j].back()] = ' ';
+                buckets[j].pop_back();
+            } else {
+                // Store the index of the character in its corresponding bucket
+                buckets[s[i] - 'a'].push_back(i);
+            }
         }
 
-        std::erase(ans, ' ');
-        return ans;
+        std::erase(ans, ' '); // Remove all marked characters (spaces)
+        return ans; // Return the resulting string
     }
 };
