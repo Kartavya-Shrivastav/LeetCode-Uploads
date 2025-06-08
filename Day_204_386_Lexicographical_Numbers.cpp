4786209e281c56ed0523225845ba7f1136417ca7
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-08 20:12:10 +0530

    Refactor lexicalOrder function for improved code formatting and consistency

--- Day_204_386_Lexicographical_Numbers.cpp
+++ Day_204_386_Lexicographical_Numbers.cpp
@@ -1,20 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+// This code defines a function to generate numbers in lexicographical order from 1 to n
+// by using a depth-first search (DFS) approach to traverse the number tree.
+
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
          vector<int> ans;
-    int curr = 1;
+        int curr = 1;
 
-    while (ans.size() < n) {
-      ans.push_back(curr);
-      if (curr * 10 <= n) {
-        curr *= 10;
-      } else {
-        while (curr % 10 == 9 || curr == n)
-          curr /= 10;
-        ++curr;
-      }
-    }
+        while (ans.size() < n) {
+        ans.push_back(curr);
+        if (curr * 10 <= n) {
+            curr *= 10;
+        } else {
+            while (curr % 10 == 9 || curr == n)
+            curr /= 10;
+            ++curr;
+        }
+        }
 
-    return ans;
+        return ans;
     }
 };
