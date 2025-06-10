b918c16ffd51fb2e14211f00cbbd195c5473815a
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-10 10:32:18 +0530

    Refactor maxDifference function for improved code readability and consistency

--- Day_206_3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
+++ Day_206_3442_Maximum_Difference_Between_Even_and_Odd_Frequency_I.cpp
@@ -1,13 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Class to find the maximum difference between the highest and lowest frequency of characters in a string
+
 class Solution {
 public:
+    // Function to find the maximum difference between the highest odd frequency
+    // and the lowest even frequency of characters in the string
     int maxDifference(string s) {
-        vector<int> mpp(26);
-        int maxi = 0, mini = s.size();
-        for (char c : s) mpp[c - 'a']++;
+        vector<int> mpp(26); // Frequency map for each lowercase letter
+        int maxi = 0, mini = s.size(); // Initialize max odd and min even frequencies
+        for (char c : s) mpp[c - 'a']++; // Count frequency of each character
         for (int i = 0 ; i < 26; i++) {
-            if (mpp[i] % 2 != 0) maxi = max(maxi, mpp[i]);
-            if (mpp[i] % 2 == 0 && mpp[i] > 0) mini = min(mini, mpp[i]);
+            if (mpp[i] % 2 != 0) // If frequency is odd
+                maxi = max(maxi, mpp[i]);
+            if (mpp[i] % 2 == 0 && mpp[i] > 0) // If frequency is even and non-zero
+                mini = min(mini, mpp[i]);
         } 
-        return maxi - mini;
+        return maxi - mini; // Return the difference
     }
 };
