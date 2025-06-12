52cec2c0f9f781413bc6b9fcadd4d0cd2c911268
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-12 20:34:35 +0530

    Add comments for clarity in maxAdjacentDistance function

--- Day_208_3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.cpp
+++ Day_208_3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.cpp
@@ -1,9 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
+    // Function to find the maximum absolute difference between adjacent elements in a circular array
     int maxAdjacentDistance(vector<int>& nums) {
         int maxi = 0;
+        // Iterate through the array and check the difference between each pair of adjacent elements
         for (int i = 0 ; i < nums.size() - 1 ; i++)
             maxi = max(maxi , abs(nums[i + 1] - nums[i]));
+        // Check the difference between the last and first element to account for the circular nature
         maxi = max(maxi , abs(nums[nums.size() - 1] - nums[0]));
         return maxi;
     }
