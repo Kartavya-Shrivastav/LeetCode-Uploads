7ac69d6d097789804323637aabe0f82084262703
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-14 23:41:05 +0530

    Implement Longest Even Length Substring solution to calculate the maximum length of even-length substrings with equal digit sums

---
+++ Longest_Even_Length_Substring.cpp
@@ -0,0 +1,46 @@
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    string str;
+	    int left=0;
+	    int c=0;
+	    int right = 0;
+	    int maxx=0;
+	    cin>>str;
+	    int n = str.length();
+	    
+	    if(n==1)
+	    {
+	         cout<<0<<endl;
+	    }
+	    
+	    else{
+	    for(int i=0;i<n;i++)
+	    {
+	        int j = i;
+	        int k = i+1;
+	        left=0;
+	        right=0;
+	        
+	        while(j>=0 && k<n)
+	        {
+	            left += str[j]-'0';
+	            right += str[k] - '0';
+	            if(left==right)
+	            {
+	                maxx = max(maxx,k-j+1);
+	            }
+	            j--;
+	            k++;
+	        }
+	    }
+	    
+	    cout<<maxx<<endl;
+	    }
+	    
+    }
+	return 0;
+}
