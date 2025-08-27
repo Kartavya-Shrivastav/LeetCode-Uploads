86cfa4b529260b34220b5e2f78e4b3bf3b29685a
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-08-27 23:43:44 +0530

    feat: enhance comments for clarity in V-shaped diagonal segment calculation

--- Day_284_3459_Length_of_Longest_V_Shaped_Diagonal_Segment.cpp
+++ Day_284_3459_Length_of_Longest_V_Shaped_Diagonal_Segment.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 class Solution {
 public:
+    // Function to find the length of longest V-shaped diagonal segment
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
+        // Define directions: down-right, down-left, up-left, up-right
         int dirs[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+        // Memoization array to store computed results
         int memo[m][n][4][2];
         memset(memo, -1, sizeof(memo));
 
+        // DFS function to explore diagonal paths
+        // cx, cy: current coordinates
+        // direction: current direction of movement
+        // turn: whether we can make a turn
+        // target: target value we're looking for (alternates between 1 and 2)
         function<int(int, int, int, bool, int)> dfs =
             [&](int cx, int cy, int direction, bool turn, int target) -> int {
             int nx = cx + dirs[direction][0];
@@ -19,25 +27,28 @@ public:
                 grid[nx][ny] != target) {
                 return 0;
             }
+            // Return memoized result if available
             if (memo[nx][ny][direction][turn] != -1) {
                 return memo[nx][ny][direction][turn];
             }
 
-            /* Continue walking in the original direction. */
+            // Try continuing in the same direction
             int maxStep = dfs(nx, ny, direction, turn, 2 - target);
             if (turn) {
-                /* Clockwise rotate 90 degrees turn */
+                // If turn is allowed, try making a 90-degree clockwise turn
                 maxStep = fmax(maxStep, dfs(nx, ny, (direction + 1) % 4, false,
                                             2 - target));
             }
+            // Store and return result
             memo[nx][ny][direction][turn] = maxStep + 1;
             return maxStep + 1;
         };
 
+        // Find maximum V-shaped path length
         int res = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
+                if (grid[i][j] == 1) {  // Start from cells with value 1
                     for (int direction = 0; direction < 4; ++direction) {
                         res = fmax(res, dfs(i, j, direction, true, 2) + 1);
                     }
