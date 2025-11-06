534bf7102cf5bf0994364604f9aafc8e3a6f1ebf
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-11-06 23:47:27 +0530

    refactor: enhance code structure and comments for modifiedList function

--- Day_350_3217_Delete_Nodes_From_Linked_List_Present_in_Array.cpp
+++ Day_350_3217_Delete_Nodes_From_Linked_List_Present_in_Array.cpp
@@ -1,3 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: Delete Nodes From Linked List Present in Array
+// Approach: Use a hash set to store values from the array for O(1) lookup
+
+// Steps:
+// 1. Store all values from the array in an unordered_set for quick access
+// 2. Traverse the linked list and remove nodes whose values are in the set
+// 3. Return the modified linked list
+
+// Time Complexity: O(n + m) where n is the number of nodes in the linked list and m is the size of the array
+// Space Complexity: O(m) for storing the values in the set
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
