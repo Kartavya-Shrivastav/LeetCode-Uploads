3a322d7e5f0c2cda28be5dba5bce98ebdace1c14
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-11-13 21:51:08 +0530

    docs: enhance comments and documentation for minOperations function

--- Day_361_2654_Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1.cpp
+++ Day_361_2654_Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1.cpp
@@ -1,3 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: Given an array of integers, find the minimum number of operations required to make all elements equal to 1.
+// An operation consists of choosing a subarray and replacing it with its GCD (Greatest Common Divisor).
+
+// Approach: First, count how many elements are already 1. If there are any, the answer is simply the size of the array minus this count.
+// If there are no 1's, we need to find the shortest subarray with GCD equal to 1.
+// We can do this by iterating through all possible subarrays, computing their GCD, and checking if it equals 1.
+
+
+// Steps:
+// 1. Count the number of elements that are already 1. If there are any, the answer is simply the size of the array minus this count.
+// 2. If there are no 1's, we need to find the shortest subarray with GCD equal to 1.
+// 3. Iterate through all possible subarrays, compute their GCD, and if it equals 1, update the result with the minimum length found.
+// 4. The final result will be the length of the shortest subarray with GCD 1 plus the size of the array minus 1 (to account for the operations needed to convert the rest of the elements).
+
+// Time Complexity: O(n^3) in the worst case due to the nested loops for subarray GCD calculation.
+// Space Complexity: O(1) as we are using only a few extra variables.
+
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
