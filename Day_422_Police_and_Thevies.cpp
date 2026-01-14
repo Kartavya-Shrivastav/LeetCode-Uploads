3fe11ad5b1f44c0cceac56715684d0340878a36e
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-01-14 10:58:30 +0530

    feat: implement catchThieves function to count thieves caught by police

---
+++ Day_422_Police_and_Thevies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int catchThieves(vector<char> &arr, int k) {
+        // Code here
+        int n=arr.size();
+        int count=0;
+        int t=0,p=0;
+        while(t<n && p<n)
+        { 
+            while(t<n && arr[t]!='T') ++t;
+            while(p<n && arr[p]!='P') ++p;
+            if(t<n && p<n && abs(t-p)<=k)
+            {
+                ++count;++p;++t;
+            }
+            else if(t<p) ++t;
+            else ++p;
+        }
+        return count;
+    }
+};
