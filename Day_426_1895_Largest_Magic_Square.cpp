180a795b92674f41d3c67f29d59d9bfcf5d5ee34
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-01-18 23:53:24 +0530

    feat: refactor isValid function to isMagic using prefix sums for magic square validation

--- Day_426_1895_Largest_Magic_Square.cpp
+++ Day_426_1895_Largest_Magic_Square.cpp
@@ -1,42 +1,58 @@
 class Solution {
-    bool isValid(vector<vector<int>>& g, int i, int j, int k) {
-        int sum = 0;
-
-        for (int x = i; x < i + k; x++) {
-            int s = 0;
-            for (int y = j; y < j + k; y++) s += g[x][y];
-            if (x == i) sum = s;
-            else if (sum != s) return false;
+public:
+    bool isMagic(vector<vector<array<int,4>>> const & prefixSum, int r, int c, int sz) {
+        int sum = prefixSum[r+sz][c+sz][2] - prefixSum[r][c][2]; // main-diagonal sum
+        // cout << " main diagonal sum: " << sum << endl;
+        if (sum != prefixSum[r+sz][c+1][3] - prefixSum[r][c+sz+1][3]) {
+            // cout << " secondary: " << "(" << r+sz << "," << c+1 << ") ";
+            // cout << prefixSum[r+sz][c+1][3] << " - "; 
+            // cout << "(" << r << "," << c+sz+1 << ") ";
+            // cout << prefixSum[r][c+sz+1][3] << endl;
+            return false; // secondary-diagonal sum
         }
+        // cout << " diagonal sum ok" << endl;
 
-        for (int y = j; y < j + k; y++) {
-            int s = 0;
-            for (int x = i; x < i + k; x++) s += g[x][y];
-            if (sum != s) return false;
+        // check row sum
+        for (int i = 0, j = r; i < sz; i++, j++) {
+            if (sum != prefixSum[j+1][c+sz][0] - prefixSum[j+1][c][0]) {
+                // cout << " row: " << j << " " << prefixSum[j+1][c+sz][0] << " - " << prefixSum[j+1][c][0] << endl;
+                return false;
+            }
         }
-
-        int s = 0;
-        for (int d = 0; d < k; d++) s += g[i + d][j + d];
-        if (sum != s) return false;
-
-        s = 0;
-        for (int d = 0; d < k; d++) s += g[i + d][j + k - 1 - d];
-        if (sum != s) return false;
-
+        // cout << " row sum ok" << endl;
+        // check col sum
+        for (int i = 0, j = c; i < sz; i++, j++) {
+            if (sum != prefixSum[r+sz][j+1][1] - prefixSum[r][j+1][1]) return false;
+        }
+        // cout << " col sum ok" << endl;
         return true;
     }
 
-public:
     int largestMagicSquare(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), res = 1;
+        int m = grid.size(), n = grid[0].size();
+        // {0,1,2,3} = {row,col,diag,anti-diag}
+        vector<vector<array<int,4>>> prefixSum(m+1, vector<array<int,4>>(n+2));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                int val = grid[i-1][j-1];
+                prefixSum[i][j][0] = prefixSum[i][j-1][0] + val;
+                prefixSum[i][j][1] = prefixSum[i-1][j][1] + val;
+                prefixSum[i][j][2] = prefixSum[i-1][j-1][2] + val;
+                prefixSum[i][j][3] = prefixSum[i-1][j+1][3] + val;
+                // cout << i << "," << j << " (" << prefixSum[i][j][3] << ") ";
+            }
+            // cout << endl;
+        }
 
-        for (int k = 2; k <= min(m, n); k++) {
+        int sz = min(m, n); // the largest possible length of magic square
+        for (int k = sz; k >= 2; k--) {
             for (int i = 0; i + k <= m; i++) {
                 for (int j = 0; j + k <= n; j++) {
-                    if (isValid(grid, i, j, k)) res = k;
+                    // cout << i << "," << j << "; sz: " << k << endl;
+                    if (isMagic(prefixSum, i, j, k)) return k;
                 }
             }
         }
-        return res;
+        return 1;
     }
 };
