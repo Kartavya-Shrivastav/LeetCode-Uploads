e33094e19cc31798936961b8e7408c0f09d1ae40
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-01-21 13:57:55 +0530

    feat: add alternative implementation for bulbSwitch function with detailed logic

--- 319_Buld_Switcher.cpp
+++ 319_Buld_Switcher.cpp
@@ -1,6 +1,29 @@
+
+
+
 class Solution {
 public:
     int bulbSwitch(int n) {
         return sqrt(n);
+    }
+};
+
+// OR
+
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        if(n == 0) return 0;
+
+        int count = 0;
+        for(int i=1; i<=n; i++){
+            if(i == (int)sqrt(i) * (int)sqrt(i)){
+                cout << i << " ";
+                count++;
+            }
+        }
+
+        return count;
     }
 };
