a077dd96ffbd4dc2bc1ec8e5c4f98b4eb5d9ea23
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-02-08 22:27:21 +0530

    refactor: improve TreeNode struct and update isBalanced function for clarity

--- Day_447_110_Balanced_Binary_Tree.cpp
+++ Day_447_110_Balanced_Binary_Tree.cpp
@@ -1,3 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
 public:
@@ -7,13 +18,11 @@ public:
         return (1 + max(height(node->left), height(node->right)));
     }
     bool isBalanced(TreeNode* root) {
-        if(root == NULL) return 1;
+        if(root == nullptr) return true;
 
         int height_l = height(root->left);
         int height_r = height(root->right);
 
         return (abs(height_l - height_r) <= 1 &&  isBalanced(root->left) && isBalanced(root->right));
-
-        return 0;
     }
 };
