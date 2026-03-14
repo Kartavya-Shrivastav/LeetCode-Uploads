14dd7b9d6eb8f8ab910e8d5d14721a9216da584a
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-03-14 21:06:58 +0530

    docs: add detailed problem description and approach for maximizing spanning tree stability

--- Day_478_3600. Maximize Spanning Tree Stability with Upgrades.cpp
+++ Day_478_3600. Maximize Spanning Tree Stability with Upgrades.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Problems: Given a graph with n nodes and edges, where each edge has a weight and a type (0 for normal, 1 for must-have), and an integer k representing the number of upgrades allowed, find the maximum stability of a spanning tree that can be achieved by upgrading at most k normal edges to must-have edges. The stability of a spanning tree is defined as the minimum weight of the edges in the tree.
+Approach:   
+1. We can use a binary search approach to find the maximum stability. We will define a function check(X) that checks if it is possible to achieve a spanning tree with stability at least X by upgrading at most k normal edges.
+2. In the check function, we will first create a DSU (Disjoint Set Union) to keep track of the connected components of the graph. We will first unite all the must-have edges that have weight at least X. If any must-have edge has weight less than X, we can immediately return false since it cannot be included in the spanning tree.
+3. Next, we will unite all the normal edges that have weight at least X. This will help us to connect the components formed by the must-have edges.
+4. After processing the must-have and normal edges, we will check how many components are left  and how many upgrades we have used. If the number of components minus one is greater than k, it means we cannot connect all components with the allowed upgrades, so we return false.
+5. Finally, we will check if we can connect the remaining components using the normal edges that have weight less than X but at least X/2. If we can connect all components, we return true.
+6. We will perform a binary search on the range of possible stability values to find the maximum stability that can be achieved.    
+
+Time Complexity: O(E log W) where E is the number of edges and W is the range of edge weights due to the binary search and DSU operations.
+Space Complexity: O(n) for the DSU data structure.
+*/
+
 class Solution {
     class DSU {
         vector<int> parent;
