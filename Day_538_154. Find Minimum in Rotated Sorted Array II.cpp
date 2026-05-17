0d0161846cdb0f2464cea985ac00dc02d4f39b16
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-05-17 13:06:37 +0530

    docs: add problem statement and approach for Find Minimum in Rotated Sorted Array II

--- Day_538_154. Find Minimum in Rotated Sorted Array II.cpp
+++ Day_538_154. Find Minimum in Rotated Sorted Array II.cpp
@@ -1,3 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Problem: Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6
+Approach: We can solve this problem by using a modified binary search algorithm. Since the array is rotated, we can compare the middle element with the last element to determine which half of the array is sorted. If the middle element is greater than the last element, it means that the minimum element is in the right half of the array. Otherwise, it means that the minimum element is in the left half of the array. We can continue this process until we find the minimum element.    
+
+Steps:
+1. Initialize two pointers, left and right, to the start and end of the array, respectively.
+2. Store the last element of the array in a variable last, as it will be used for comparison.
+3. While left is less than right:
+   a. Calculate the middle index mid as the average of left and right.
+   b. If the middle element nums[mid] is greater than last, it means that the minimum element is in the right half of the array. Therefore, update left to mid + 1.
+   c. Otherwise, it means that the minimum element is in the left half of the array. Therefore, update right to mid.    
+4. After the loop, left will be pointing to the minimum element in the array, which we can return as the result.
+
+Time Complexity: O(log n), where n is the length of the nums array. The binary search algorithm allows us to find the minimum element in logarithmic time.
+Space Complexity: O(1), since we are using only a constant amount of extra space to store the pointers and the last element for comparison.
+*/
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
