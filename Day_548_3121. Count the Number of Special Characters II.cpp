3f375be2415062bb5d2f8f68b9811943929e615c
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-06-10 23:09:20 +0530

    docs: add problem statement and approach for Count the Number of Special Characters II

--- Day_548_3121. Count the Number of Special Characters II.cpp
+++ Day_548_3121. Count the Number of Special Characters II.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Problem: You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, and every lowercase occurrence of c appears before the first uppercase occurrence of c.
+Return the number of special letters in word.
+
+Approach: 
+
+Steps:
+
+
+*/
+
+
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
